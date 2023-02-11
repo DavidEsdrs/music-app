85ef0625648afe4e4fd8d6287c953c506ea2b4c6
DavidEsdrs <carpina1983@gmail.com>
2023-02-11 02:08:38 -0300

    Implements user creation and login endpoints

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,14 @@
-import { Router } from "express";
+import { Router, Request, Response } from "express";
+import AppDataSource from "./ormconfig";
+import { buildCreateUser } from "./services/users/CreateUser/buildCreateUser";
+import { buildLoginService } from "./services/users/LoginUser/buildLoginService";
+import { validateLoginUser } from "./services/users/LoginUser/LoginUser.middleware";
 
 const router = Router();
 
 router.get("/", (req, res) => res.json("running"));
+
+router.post("/users", (req, res) => buildCreateUser().handle(req, res));
+router.post("/login", validateLoginUser, (req, res) => buildLoginService().handle(req, res));
 
 export default router;
