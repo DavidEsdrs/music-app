0d95a68cbf6c3d2133ec253dd05e1dec864a4066
DavidEsdrs <carpina1983@gmail.com>
2023-02-11 22:21:06 -0300

    Use cors middleware

--- src/server.ts
+++ src/server.ts
@@ -2,13 +2,15 @@ import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
 import router from "./router";
+import cors from "cors";
 import { errorHandling } from "./middlewares/ErrorHandling";
 
 const server = express();
 
+server.use(cors());
 server.use(express.json());
-server.use(router);
 server.use(errorHandling);
+server.use(router);
 
 const SERVER_PORT = process.env.SERVER_PORT || 4343;
 
