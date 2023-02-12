3da60966c864650b596dd47987843110bf55fef0
DavidEsdrs <carpina1983@gmail.com>
2023-02-11 22:04:53 -0300

    Add playlists table creation migration

---
+++ src/migrations/1676100806799-create_playlists_table.ts
@@ -0,0 +1,27 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class createPlaylistsTable1676100806799 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+
+        await queryRunner.query(`
+            CREATE TABLE playlists(
+                idPlaylist INT PRIMARY KEY AUTO_INCREMENT,
+                title VARCHAR(50) NOT NULL,
+                description VARCHAR(255),
+                visibility ENUM("public", "private") NOT NULL DEFAULT "private",
+                path_featured_picture VARCHAR(200),
+                created_at DATETIME DEFAULT NOW(),
+                updated_at DATETIME DEFAULT NOW(),
+                released_on YEAR(4),
+                creator_fk INT NOT NULL,
+                FOREIGN KEY (creator_fk) REFERENCES users(id)
+            )
+        `);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`DROP TABLE playlists`);
+    }
+
+}
