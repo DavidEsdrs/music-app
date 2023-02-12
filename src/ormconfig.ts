b96114f1c193d98a58b792aafe0eeb912f67af87
DavidEsdrs <carpina1983@gmail.com>
2023-02-11 22:07:35 -0300

    Add entities and migrations classes to typeorm data source

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,6 +2,8 @@ import { DataSource } from "typeorm"
 import dotenv from "dotenv";
 import { createUsersTable1676058796952 } from "./migrations/1676058796952-create_users_table";
 import { User } from "./entities/User";
+import { createPlaylistsTable1676100806799 } from "./migrations/1676100806799-create_playlists_table";
+import { Playlist } from "./entities/Playlist";
 
 dotenv.config({
     path: process.env.NODE_ENV === "dev" ? ".env.dev" : ".env.production"
@@ -11,10 +13,12 @@ const AppDataSource = new DataSource({
     type: "mysql",
     url: process.env.DATABASE_URL,
     entities: [
-        User
+        User,
+        Playlist
     ],
     migrations: [
-        createUsersTable1676058796952
+        createUsersTable1676058796952,
+        createPlaylistsTable1676100806799
     ]
 });
 
