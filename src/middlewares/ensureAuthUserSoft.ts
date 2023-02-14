6df7e6c3cb87b3c0a8170935c512ab25f8a7b653
DavidEsdrs <carpina1983@gmail.com>
2023-02-13 21:30:30 -0300

    Add auth user middleware to unauthenticated user

---
+++ src/middlewares/ensureAuthUserSoft.ts
@@ -0,0 +1,26 @@
+import { NextFunction, Request, Response } from "express";
+import { verify } from "jsonwebtoken";
+import { UnauthorizedRequestError } from "../api/APIErrors";
+
+interface IPayload {
+    sub: string;
+}
+
+export const ensureAuthUserSoft = (req: Request, res: Response, next: NextFunction) => {
+    const auth = req.headers.authorization;
+    if(!auth) {
+        return next();
+    }
+    const [ , token ] = auth.split(' ');
+    if(!token) {
+        throw new UnauthorizedRequestError();
+    }
+    try {
+        const { sub } = verify(token, process.env.JWT_TOKEN) as IPayload;
+        req.user_id = Number(sub);
+        return next();
+    }
+    catch(error) {
+        throw new UnauthorizedRequestError();
+    }
+}
