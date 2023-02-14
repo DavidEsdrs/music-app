de4b29c8b13f6db36e876e2115704534c19bb175
DavidEsdrs <carpina1983@gmail.com>
2023-02-13 21:28:27 -0300

    Create Song Entity

---
+++ src/entities/Song.ts
@@ -0,0 +1,23 @@
+import { Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { Playlist } from "./Playlist";
+
+@Entity("songs")
+export class Song {
+    @PrimaryGeneratedColumn({ name: "idSong" })
+    id: number;
+
+    @Column({ name: "title", type: "varchar" })
+    title: string;
+
+    @Column({ name: "creator_fk", type: "int" })
+    creator_fk: number;
+
+    @Column({ name: "file_path", type: "varchar" })
+    file_path: string;
+
+    @CreateDateColumn({ name: "created_at" })
+    created_at: Date;
+
+    @UpdateDateColumn({ name: "updated_at" })
+    updated_at: Date;
+}
