25962bafc6b59f20d11093f504ea194b5de246f7
DavidEsdrs <carpina1983@gmail.com>
2023-02-13 21:36:14 -0300

    Create default playlist when a new user is created

--- src/services/users/CreateUser/CreateUserService.ts
+++ src/services/users/CreateUser/CreateUserService.ts
@@ -2,23 +2,52 @@ import { IUsersRepository } from "../../../repositories/UsersRepository";
 import { ICreateUserDTO } from "./CreateUserDTO";
 import { hash } from "argon2";
 import { EmailAlreadyExistsError } from "../../../api/APIErrors";
+import { IPlaylistsRepository } from "../../../repositories/PlaylistsRepository";
+import { IPlaylistUserRepository } from "../../../repositories/PlaylistUserRepository";
+
+type CreateUserServiceArgs = {
+    usersRepository: IUsersRepository,
+    playlistsRepository: IPlaylistsRepository,
+    playlistsUserRepository: IPlaylistUserRepository
+}
 
 export class CreateUserService {
     constructor(
-        private usersRepository: IUsersRepository
+        private services: CreateUserServiceArgs
     ) {}
 
     async execute(args: ICreateUserDTO) {
-        const userAlreadyExists = await this.usersRepository.findByEmail(args.email);
+        const userAlreadyExists = await this.services.usersRepository.findByEmail(args.email);
         if(userAlreadyExists) {
             throw new EmailAlreadyExistsError();
         }
-        const passwordHash = await hash(args.password);
-        const user = this.usersRepository.create({
+        const passwordHash = await hash(args.password, {
+            salt: Buffer.from(args.email)
+        });
+        const user = this.services.usersRepository.create({
             ...args,
             password: passwordHash
         });
-        await this.usersRepository.save(user);
+        const userInDb = await this.services.usersRepository.save(user);
+        await this.createDefaultPlaylist(userInDb.id);
         return user;
+    }
+    
+    private async createDefaultPlaylist(user_id: number)
+    {
+        const default_playlist = this.services.playlistsRepository.create({  
+            title: "UPLOADED_SONGS",
+            description: "Songs upload by user " + user_id,
+            visibility: "private",
+            released_on: new Date().getUTCFullYear(),
+            creator_fk: user_id
+        });
+        const playlist = await this.services.playlistsRepository.save(default_playlist);
+        const userPlaylistRelation = this.services.playlistsUserRepository.create({ 
+            playlist_id: playlist.id,
+            user_id: user_id 
+        });
+        await this.services.playlistsUserRepository.save(userPlaylistRelation);
+
     }
 }
--- src/services/users/CreateUser/buildCreateUser.ts
+++ src/services/users/CreateUser/buildCreateUser.ts
@@ -1,9 +1,15 @@
+import { TypeormPlaylistsRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistsRepository";
+import { typeormPlaylistUserRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistUser";
 import { TypeormUsersRepository } from "../../../repositories/typeorm/mysql/TypeormUsersRepository"
 import { CreateUserController } from "./CreateUserController";
 import { CreateUserService } from "./CreateUserService"
 
 export const buildCreateUser = () => {
-    const service = new CreateUserService(TypeormUsersRepository);
+    const service = new CreateUserService({
+        usersRepository: TypeormUsersRepository, 
+        playlistsRepository: TypeormPlaylistsRepository, 
+        playlistsUserRepository: typeormPlaylistUserRepository
+    });
     const controller = new CreateUserController(service);
     return controller;
 }
