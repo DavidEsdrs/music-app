9e6f3cb30767c0bc261e0bcff0a9d2bac5052ce3
DavidEsdrs <carpina1983@gmail.com>
2023-02-13 21:31:06 -0300

    Add multer parser to upload song files

---
+++ src/middlewares/songs/uploadSongFile.ts
@@ -0,0 +1,51 @@
+import path from "path";
+import fs from "fs";
+import multer from "multer";
+import mime from "mime";
+import { Request } from "express";
+import { UnprocessableEntityError } from "../../api/APIErrors";
+
+const songFileUpload = {
+    // URL in wich the uploaded file will be stored
+    URL: path.basename("uploads"),
+
+    // Set the storage estrategy used by multer 
+    storage(): multer.StorageEngine {
+        return multer.diskStorage({
+            destination: (req, file, cb) => {
+                if(!fs.existsSync(songFileUpload.URL)) {
+                    fs.mkdirSync(songFileUpload.URL);
+                }
+                cb(null, songFileUpload.URL + "/songs");
+            },
+
+            filename: (req, file, cb) => {
+                const file_type = mime.getExtension(file.mimetype);
+                const file_name = `${new Date().getTime()}.${file.originalname.split('.').pop()}`;
+                req.file_props = { file_type, file_name };
+                cb(null, file_name);
+            }
+        });
+    },
+
+    fileFilter() {
+        return (req: Request, file: Express.Multer.File, cb: multer.FileFilterCallback) => {
+            const type = mime.getExtension(file.mimetype);
+            const conditions = [ "mpga" ];
+            if(!conditions.includes(type)) {
+                return cb(new UnprocessableEntityError("The file type is invalid!"));
+            }
+            cb(null, true);
+        }
+    },
+
+    getConfig(): multer.Options {
+        return {
+            storage: songFileUpload.storage(),
+            fileFilter: songFileUpload.fileFilter()
+        }
+    }
+
+}
+
+export const validateAndParseSongFileUpload = multer(songFileUpload.getConfig()).single("song_file");
