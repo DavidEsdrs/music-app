63bbd24fd6510e07a10fe37cc8d1ca0fff5310bb
DavidEsdrs <carpina1983@gmail.com>
2023-02-13 22:05:01 -0300

    Fix service instantiation

--- src/services/playlists/CreatePlaylist/buildCreatePlaylist.ts
+++ src/services/playlists/CreatePlaylist/buildCreatePlaylist.ts
@@ -1,13 +1,14 @@
 import { PlaylistUser } from "../../../entities/PlaylistUser";
 import AppDataSource from "../../../ormconfig";
 import { TypeormPlaylistsRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistsRepository"
+import { typeormPlaylistUserRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistUser";
 import { CreatePlaylistController } from "./CreatePlaylistController";
 import { CreatePlaylistService } from "./CreatePlaylistService"
 
 export const buildCreatePlaylist = () => {
     const service = new CreatePlaylistService(
         TypeormPlaylistsRepository, 
-        AppDataSource.getRepository(PlaylistUser)
+        typeormPlaylistUserRepository
     );
     const controller = new CreatePlaylistController(service);
     return controller;
