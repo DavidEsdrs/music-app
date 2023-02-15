c72a10e0eb97c2aceca709979046e886f76a97aa
DavidEsdrs <carpina1983@gmail.com>
2023-02-15 02:38:19 -0300

    Refactor: remove unnecessary dependency injection

--- src/services/songs/CreateSong/buildCreateSong.ts
+++ src/services/songs/CreateSong/buildCreateSong.ts
@@ -1,21 +1,11 @@
 import { TypeormPlaylistsRepository as playlistsRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistsRepository";
-import { typeormPlaylistUserRepository as playlistUserRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistUser";
-import { typeormSongPlaylistsRepository } from "../../../repositories/typeorm/mysql/TypeormSongPlaylistRepository";
 import { songsRepository } from "../../../repositories/typeorm/mysql/TypeormSongsRepository";
-import { typeormSongUserRepository as songUserRepository } from "../../../repositories/typeorm/mysql/TypeormSongUserRepository";
 import { AddSongToPlaylistService } from "../../playlists/AddSongToPlaylist/AddSongToPlaylistService";
 import { CreateSongController } from "./CreateSongController";
 import { CreateSongService } from "./CreateSongService"
 
 export const buildCreateSong = () => {
-    const addSongToPlaylist = new AddSongToPlaylistService(typeormSongPlaylistsRepository, playlistsRepository);
-    const service = new CreateSongService({
-        addSongToPlaylist,
-        playlistsRepository,
-        playlistUserRepository,
-        songsRepository,
-        songUserRepository
-    });
+    const service = new CreateSongService(songsRepository);
     const controller = new CreateSongController(service);
     return controller;
 }
