3300609e85f42a67a932d323b2cb45b2b4417058
DavidEsdrs <carpina1983@gmail.com>
2023-02-15 02:40:35 -0300

    Refactor: remove unnecessary imports and dependencies injections

--- src/services/playlists/CreatePlaylist/CreatePlaylistService.ts
+++ src/services/playlists/CreatePlaylist/CreatePlaylistService.ts
@@ -1,21 +1,14 @@
-import { IPlaylistUserRepository } from "../../../repositories/PlaylistUserRepository";
 import { IPlaylistsRepository } from "../../../repositories/PlaylistsRepository";
 import { ICreatePlaylistDTO } from "./CreatePlaylistDTO";
 
 export class CreatePlaylistService {
     constructor(
-        private playlistsRepository: IPlaylistsRepository,
-        private playlistUserRepository: IPlaylistUserRepository
+        private playlistsRepository: IPlaylistsRepository
     ) {}
 
     async execute(args: ICreatePlaylistDTO) {
         const playlist = this.playlistsRepository.create(args);
-        const createdPlaylist = await this.playlistsRepository.save(playlist);
-        const relationPlaylistUser = this.playlistUserRepository.create({ 
-            playlist_id: createdPlaylist.id, 
-            user_id: args.creator_fk
-        });
-        await this.playlistUserRepository.save(relationPlaylistUser);
+        const createdPlaylist = await this.playlistsRepository.savePlaylist(playlist);
         return createdPlaylist;
     }
 }
--- src/services/playlists/CreatePlaylist/buildCreatePlaylist.ts
+++ src/services/playlists/CreatePlaylist/buildCreatePlaylist.ts
@@ -1,15 +1,9 @@
-import { PlaylistUser } from "../../../entities/PlaylistUser";
-import AppDataSource from "../../../ormconfig";
 import { TypeormPlaylistsRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistsRepository"
-import { typeormPlaylistUserRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistUser";
 import { CreatePlaylistController } from "./CreatePlaylistController";
 import { CreatePlaylistService } from "./CreatePlaylistService"
 
 export const buildCreatePlaylist = () => {
-    const service = new CreatePlaylistService(
-        TypeormPlaylistsRepository, 
-        typeormPlaylistUserRepository
-    );
+    const service = new CreatePlaylistService(TypeormPlaylistsRepository);
     const controller = new CreatePlaylistController(service);
     return controller;
 }
