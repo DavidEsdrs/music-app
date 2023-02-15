fdc5b5ca1432d6846dd5ea48e51a89a062cace2a
DavidEsdrs <carpina1983@gmail.com>
2023-02-15 02:43:07 -0300

    Only add song to another playlist if it is public

--- src/services/playlists/AddSongToPlaylist/AddSongToPlaylistService.ts
+++ src/services/playlists/AddSongToPlaylist/AddSongToPlaylistService.ts
@@ -2,24 +2,39 @@ import { UnauthorizedRequestError } from "../../../api/APIErrors";
 import { ResponseEntity } from "../../../api/ResponseEntity";
 import { SongPlaylist } from "../../../entities/SongPlaylist";
 import { IPlaylistsRepository } from "../../../repositories/PlaylistsRepository";
-import { ISongPlaylistRepository } from "../../../repositories/SongPlaylistRepository";
 import { ISongsRepository } from "../../../repositories/SongsRepository";
+import { IAddSongToPlaylistDTO } from "./AddSongToPlaylistDTO";
 
 export class AddSongToPlaylistService {
     constructor(
-        private songPlaylistRepository: ISongPlaylistRepository,
-        private playlistsRepository: IPlaylistsRepository
+        private playlistsRepository: IPlaylistsRepository,
+        private songsRepository: ISongsRepository
     ) {}
 
-    async execute({ user_id, song_id, playlist_id }) {
+    async execute({ user_id, song_id, playlist_id }: IAddSongToPlaylistDTO) {
         const playlist = await this.playlistsRepository.findById(playlist_id);
         if(playlist.creator_fk !== user_id) {
             throw new UnauthorizedRequestError();
         }
-        const songPlaylistRel = this.songPlaylistRepository.create({ playlist_id, song_id });
-        await this.songPlaylistRepository.save(songPlaylistRel);
-        const response = new ResponseEntity<SongPlaylist>("The song " + song_id + " was added to " + playlist_id, 200);
-        response.updated = songPlaylistRel;
-        return response;
+
+        let result: ResponseEntity<SongPlaylist>;
+        
+        if(playlist.creator_fk === user_id) {
+            await this.playlistsRepository.addSongToPlaylist(playlist_id, song_id);
+            result = new ResponseEntity<SongPlaylist>("Song successfully added to playlist", 200);
+            return result;
+        }
+
+        const isPublicSong = await this.songsRepository.isPublicSong(song_id);
+
+        if(!isPublicSong) {
+            throw new UnauthorizedRequestError();
+        }
+
+        await this.playlistsRepository.addSongToPlaylist(playlist_id, song_id);
+
+        result = new ResponseEntity<SongPlaylist>("Song successfully added to playlist", 200);
+
+        return result;
     }
 }
--- src/services/playlists/AddSongToPlaylist/builAddSongToPlaylist.ts
+++ src/services/playlists/AddSongToPlaylist/builAddSongToPlaylist.ts
@@ -1,10 +1,10 @@
 import { TypeormPlaylistsRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistsRepository"
-import { typeormSongPlaylistsRepository } from "../../../repositories/typeorm/mysql/TypeormSongPlaylistRepository"
+import { songsRepository } from "../../../repositories/typeorm/mysql/TypeormSongsRepository";
 import { AddSongToPlaylistController } from "./AddSongToPlaylistController";
 import { AddSongToPlaylistService } from "./AddSongToPlaylistService"
 
 export const buildAddSongToPlaylist = () => {
-    const service = new AddSongToPlaylistService(typeormSongPlaylistsRepository, TypeormPlaylistsRepository);
+    const service = new AddSongToPlaylistService(TypeormPlaylistsRepository, songsRepository);
     const controller = new AddSongToPlaylistController(service);
     return controller;
 }
