ed426832891ec69991e52d1f210f9b4871fa6ddd
DavidEsdrs <carpina1983@gmail.com>
2023-02-15 02:42:03 -0300

    Refactor: remove unnecessary imports and dependencies injections

--- src/services/playlists/GetPlaylistsFromUser/buildGetPlaylistsFromUser.ts
+++ src/services/playlists/GetPlaylistsFromUser/buildGetPlaylistsFromUser.ts
@@ -1,10 +1,9 @@
 import { TypeormPlaylistsRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistsRepository";
-import { typeormPlaylistUserRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistUser";
 import { GetPlaylistsFromUserController } from "./GetPlaylistsFromUserController";
 import { GetPlaylistsFromUserService } from "./GetPlaylistsFromUserService"
 
 export const buildGetPlaylistsFromUser = () => {
-    const service = new GetPlaylistsFromUserService(TypeormPlaylistsRepository,typeormPlaylistUserRepository);
+    const service = new GetPlaylistsFromUserService(TypeormPlaylistsRepository);
     const controller = new GetPlaylistsFromUserController(service);
     return controller;
 }
