410f72ef27cc39fdb4033c7ac93117ad6b8fcdba
DavidEsdrs <carpina1983@gmail.com>
2023-02-21 06:56:44 -0300

    Foreign keys cascades

--- src/migrations/1676255861532-create_songs_users_join_table.ts
+++ src/migrations/1676255861532-create_songs_users_join_table.ts
@@ -9,8 +9,12 @@ export class createSongsUsersJoinTable1676255861532 implements MigrationInterfac
                 song_id INT NOT NULL,
                 user_id INT NOT NULL,
                 PRIMARY KEY(song_id, user_id),
-                FOREIGN KEY (user_id) REFERENCES users(idUser),
-                FOREIGN KEY (song_id) REFERENCES songs(idSong)
+                FOREIGN KEY (user_id) 
+                    REFERENCES users(idUser)
+                    ON DELETE CASCADE,
+                FOREIGN KEY (song_id) 
+                    REFERENCES songs(idSong)
+                    ON DELETE CASCADE
             )
         `);
     }
