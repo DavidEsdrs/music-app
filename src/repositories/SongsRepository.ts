fef2af114e63332ba99658dc782903a0ca2cb19c
DavidEsdrs <carpina1983@gmail.com>
2023-02-24 01:10:44 -0300

    Join song function

--- src/repositories/SongsRepository.ts
+++ src/repositories/SongsRepository.ts
@@ -1,8 +1,16 @@
+import { Playlist } from "../entities/Playlist";
 import { Song } from "../entities/Song";
+import { User } from "../entities/User";
 import { GenericRepository } from "./GenericRepository";
+
+interface __SongJoin {
+    playlists: Partial<Playlist>[];
+}
+
+export type SongJoin = Partial<Song & __SongJoin>;
 
 export interface ISongsRepository extends GenericRepository<Song> {
     saveSong(song: Partial<Song>): Promise<Song>;
-    isPublicSong(song_id: number): Promise<boolean>;
     deleteSong(id: number): Promise<void>;
+    joinSongPlaylistUser(song_id: number): Promise<SongJoin>;
 }
