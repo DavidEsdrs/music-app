a04ddaf7175e9840d9d59101203bbf36832ef01e
DavidEsdrs <carpina1983@gmail.com>
2023-02-24 01:08:07 -0300

    Use promise.all for better performance && select needed columns

--- src/repositories/typeorm/mysql/TypeormPlaylistsRepository.ts
+++ src/repositories/typeorm/mysql/TypeormPlaylistsRepository.ts
@@ -21,8 +21,8 @@ export const TypeormPlaylistsRepository = AppDataSource.getRepository(Playlist).
         return savedRels;
     },
 
-    async findById(id: number) {
-        const playlist = await this.findOneBy({ id });
+    async findById(idPlaylist: number) {
+        const playlist = await this.findOneBy({ idPlaylist });
         return playlist;
     },
 
@@ -43,10 +43,10 @@ export const TypeormPlaylistsRepository = AppDataSource.getRepository(Playlist).
 
     async findSongsByPlaylist(id: number) {
         const q = await this.query(`
-            SELECT songs.idSong AS id, idPlaylist AS playlist_id, songs.title AS song_title, songs.file_path AS file_path, songs.created_at AS created_at, songs.updated_at AS updated_at, songs.creator_fk AS song_creator_fk
+            SELECT idSong, songs.title AS song_title, songs.file_path
             FROM playlists
-            INNER JOIN songs_playlists AS sp ON idPlaylist=sp.playlist_id
-            INNER JOIN songs ON sp.song_id=songs.idSong
+            INNER JOIN songs_playlists AS sp ON sp.playlist_id=idPlaylist
+            INNER JOIN songs ON idSong=sp.song_id
             WHERE idPlaylist=${id}
         `);
         return q;
@@ -91,36 +91,42 @@ export const TypeormPlaylistsRepository = AppDataSource.getRepository(Playlist).
 
         const result = await this.manager.transaction(async (manager: EntityManager) => {
             const playlist = await manager.query(`
-                SELECT idPlaylist, title, description, visibility, released_on, path_featured_picture, creator_fk, created_at, updated_at, songs.id AS songs_id
+                SELECT idPlaylist, playlists.title AS playlist_title, description, visibility, released_on, path_featured_picture, playlists.creator_fk, playlists.created_at, playlists.updated_at, songs.idSong AS songs_id
                 FROM playlists
                 INNER JOIN songs_playlists AS sp ON sp.playlist_id=${playlist_to_copy}
                 INNER JOIN songs ON idSong=sp.song_id
                 WHERE idPlaylist=${playlist_to_copy}
             `);
-    
-            const copyPlaylist: Playlist = await manager.query(`
+
+            const songs_ids = playlist.map((p: any)=> p.songs_id);
+
+            const copyPlaylist = await manager.query(`
                 INSERT INTO playlists(title, description, visibility, released_on, path_featured_picture, creator_fk) VALUES(
-                    ${playlist.title},
-                    ${playlist.description ? `"${playlist.description}"` : `NULL`},
-                    ${playlist.visibility ? `"${playlist.visibility}"` : `"private"`},
-                    ${playlist.released_on ?? `${new Date().getUTCFullYear()}`},
-                    ${playlist.path_featured_picture ? `"${playlist.path_featured_picture}"` : `NULL`},
+                    "${playlist[0].playlist_title}",
+                    ${playlist[0].description ? `"${playlist[0].description}"` : `NULL`},
+                    ${playlist[0].visibility ? `"${playlist[0].visibility}"` : `"private"`},
+                    ${playlist[0].released_on ?? `${new Date().getUTCFullYear()}`},
+                    ${playlist[0].path_featured_picture ? `"${playlist[0].path_featured_picture}"` : `NULL`},
                     ${copy_owner_id}
                 )
             `);
-    
-            playlist.songs_id.forEach(async (song_id: number) => {
-                await manager.query(`
+
+            const promises = songs_ids.map((song_id: number) => {
+                const p = manager.query(`
                     INSERT INTO songs_playlists(song_id, playlist_id) VALUES(
                         ${song_id},
-                        ${copyPlaylist.id}
+                        ${copyPlaylist.insertId}
                     )
-                `);
+                `)
+
+                return p;
             });
-    
-            const copyPlaylistUserRel: PlaylistUser = await manager.query(`
-                    INSERT INTO playlists_users(playlist_id, user_id) VALUES(${copyPlaylist.id}, ${copy_owner_id})
+
+            const copyPlaylistUserRel = manager.query(`
+                    INSERT INTO playlists_users(playlist_id, user_id) VALUES(${copyPlaylist.insertId}, ${copy_owner_id})
             `);
+
+            await Promise.all([ ...promises, copyPlaylistUserRel ]);
     
             return copyPlaylist;
         });
@@ -130,7 +136,7 @@ export const TypeormPlaylistsRepository = AppDataSource.getRepository(Playlist).
 
     async findPlaylistsByUser(user_id: number) {
         const playlists = await this.query(`
-            SELECT idPLaylist AS id, users.idUser, title, path_featured_picture, visibility, description, released_on, playlists.created_at, playlists.updated_at, creator_fk
+            SELECT idPlaylist, users.idUser, title, path_featured_picture, visibility, description, released_on, playlists.created_at, playlists.updated_at, creator_fk
             FROM users
             INNER JOIN playlists_users AS pu ON idUser=pu.user_id
             INNER JOIN playlists ON pu.playlist_id=playlists.idPlaylist
@@ -141,9 +147,19 @@ export const TypeormPlaylistsRepository = AppDataSource.getRepository(Playlist).
     },
 
     async addSongToPlaylist(playlist_id: number, song_id: number) {
-        await this.query(`
-            INSERT INTO songs_playlists(playlist_id, song_id) VALUES(${playlist_id}, ${song_id})
-        `);
+        await this.manager.transaction(async (manager: EntityManager) => {
+            const spInsert = manager.query(`
+                INSERT INTO songs_playlists(playlist_id, song_id) VALUES(${playlist_id}, ${song_id})
+            `);
+
+            const playlistsUpdate = manager.query(`
+                UPDATE playlists
+                SET updated_at=NOW()
+                WHERE idPlaylist=${playlist_id}
+            `);
+
+            await Promise.all([ spInsert, playlistsUpdate ]);
+        });
     },
 
     async deletePlaylist(playlist_id: number) {
