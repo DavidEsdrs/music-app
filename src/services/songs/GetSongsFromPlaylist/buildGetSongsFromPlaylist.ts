209ffb24f70b1a95a07e3ccd6638fb420b05744e
DavidEsdrs <carpina1983@gmail.com>
2023-02-24 01:44:27 -0300

    Parses ffmpeg file handling instanc

--- src/services/songs/GetSongsFromPlaylist/buildGetSongsFromPlaylist.ts
+++ src/services/songs/GetSongsFromPlaylist/buildGetSongsFromPlaylist.ts
@@ -1,9 +1,10 @@
 import { TypeormPlaylistsRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistsRepository";
+import { FFmpegFileHandling } from "../../../utils/fileHandling/FileHandling";
 import { GetSongsFromPlaylistService } from "./GetSongsFromPlaylist"
 import { GetSongsFromPlaylistController } from "./GetSongsFromPlaylistController";
 
 export const buildGetSongsFromPlaylist = () => {
-    const service = new GetSongsFromPlaylistService(TypeormPlaylistsRepository);
+    const service = new GetSongsFromPlaylistService(TypeormPlaylistsRepository, new FFmpegFileHandling());
     const controller = new GetSongsFromPlaylistController(service);
     return controller;
 }
