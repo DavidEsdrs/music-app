9d6e6b9d5df07af3918d3a7a8645bbe00a439938
DavidEsdrs <carpina1983@gmail.com>
2023-02-24 01:05:07 -0300

    Uses id's as in the database

--- src/utils/fulfillInfo.ts
+++ src/utils/fulfillInfo.ts
@@ -1,6 +1,27 @@
 import { Playlist } from "../entities/Playlist";
+import { Song } from "../entities/Song";
 import { User } from "../entities/User";
+import { SongWithDownloadLink } from "../services/songs/GetSongsFromPlaylist/GetSongsFromPlaylistController";
+import { FileHandling } from "./fileHandling/FileHandling";
 
-export const fulfillPlaylist = (playlist: Playlist) => ({ ...playlist, songs_url: `${process.env.API_URL}/playlist/${playlist.id}/song` }) as Playlist;
+export const fulfillPlaylist = (playlist: Playlist) => ({ 
+    ...playlist, 
+    songs_url: `${process.env.API_URL}/playlist/${playlist.idPlaylist}/song`, 
+    playlist_url: `${process.env.API_URL}/playlist/${playlist.idPlaylist}` 
+}) as Playlist;
 
-export const fulfillUser = (user: User) => ({ ...user, playlists_url: `${process.env.API_URL}/user/${user.id}/playlist`, password: undefined }) as User;
+export const fulfillUser = (user: User) => ({ 
+    ...user, 
+    playlists_url: `${process.env.API_URL}/user/${user.idUser}/playlist`, 
+    password: undefined 
+}) as User;
+
+export const fulfillSong = async (song: Song, fileHandling: FileHandling) => { 
+    const duration = await fileHandling.getSongDuration(fileHandling.getPath(song.file_path));
+
+    return {
+        ...song, 
+        download_link: `${process.env.API_URL}/song/${song.idSong}/download`, 
+        duration 
+    } as SongWithDownloadLink
+};
