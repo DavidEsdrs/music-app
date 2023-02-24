c7c9a47f71eed37d8938384b554c886954150d1a
DavidEsdrs <carpina1983@gmail.com>
2023-02-24 01:09:24 -0300

    Add join function

--- src/repositories/typeorm/mysql/TypeormSongsRepository.ts
+++ src/repositories/typeorm/mysql/TypeormSongsRepository.ts
@@ -1,11 +1,13 @@
 import { EntityManager } from "typeorm";
-import { APIErrors } from "../../../api/APIErrors";
+import { APIErrors, UnauthorizedRequestError } from "../../../api/APIErrors";
+import { Playlist } from "../../../entities/Playlist";
 import { Song } from "../../../entities/Song";
 import AppDataSource from "../../../ormconfig";
+import { SongJoin } from "../../SongsRepository";
 
 export const songsRepository = AppDataSource.getRepository(Song).extend({
-    async findById(id: number) {
-        const song = await this.findOneBy({ id });
+    async findById(idSong: number) {
+        const song = await this.findOneBy({ idSong });
         return song;
     },
 
@@ -25,13 +27,13 @@ export const songsRepository = AppDataSource.getRepository(Song).extend({
                 `);
 
                 const song = (await manager.query(`
-                    SELECT idSong AS id, songs.title
+                    SELECT idSong, songs.title
                     FROM songs 
                     WHERE idSong=${songInDb.insertId}
                 `))[0];
 
                 await manager.query(`
-                    INSERT INTO songs_users(user_id, song_id) VALUES (${song_partial.creator_fk}, ${song.id})
+                    INSERT INTO songs_users(user_id, song_id) VALUES (${song_partial.creator_fk}, ${song.idSong})
                 `);
 
                 const default_playlist = (await manager.query(`
@@ -41,7 +43,7 @@ export const songsRepository = AppDataSource.getRepository(Song).extend({
                 `))[0];
 
                 await manager.query(`
-                    INSERT INTO songs_playlists(playlist_id, song_id) VALUES(${default_playlist.idPlaylist}, ${song.id})
+                    INSERT INTO songs_playlists(playlist_id, song_id) VALUES(${default_playlist.idPlaylist}, ${song.idSong})
                 `);
 
                 return song;
@@ -55,17 +57,29 @@ export const songsRepository = AppDataSource.getRepository(Song).extend({
         return insertSong;
     },
 
-    async isPublicSong(song_id: number) {
-        const songs = await this.query(`
-            SELECT sp.playlist_id
+    async joinSongPlaylistUser(song_id: number) {
+        const song = await this.query(`
+            SELECT idSong, songs.title AS song_title, users.idUser, playlists.idPlaylist, playlists.title AS playlist_title, playlists.visibility, file_path
             FROM songs
             INNER JOIN users ON songs.creator_fk=users.idUser
             INNER JOIN songs_playlists AS sp ON songs.idSong=sp.song_id
             INNER JOIN playlists ON sp.playlist_id=playlists.idPlaylist
-            WHERE idSong=${song_id} AND playlists.visibility="public";
+            WHERE idSong=${song_id}
         `);
 
-        return !!songs;
+        if(song.length <= 0) {
+            throw new UnauthorizedRequestError();
+        }
+
+        const song_join: SongJoin = {
+            idSong: song[0].idSong,
+            creator_fk: song[0].idUser,
+            title: song[0].song_title,
+            file_path: song[0].file_path,
+            playlists: song.map((s: any) => ({ idPlaylist: s.idPlaylist, title: s.playlist_title, visibility: s.visibility }))
+        };
+
+        return song_join;
     },
 
     async deleteSong(id: number) {
