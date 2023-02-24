e5aa0c73773419227cd4878907a51a331d1c37a2
DavidEsdrs <carpina1983@gmail.com>
2023-02-24 01:24:36 -0300

    Returns fulfilled playlists

--- src/services/playlists/GetPlaylistsFromUser/GetPlaylistsFromUserService.ts
+++ src/services/playlists/GetPlaylistsFromUser/GetPlaylistsFromUserService.ts
@@ -1,6 +1,7 @@
 import { Playlist } from "../../../entities/Playlist";
 import { IPlaylistsRepository } from "../../../repositories/PlaylistsRepository";
 import { IGetPlaylistsFromUserDTO } from "./GetPlaylistsFromUserDTO";
+import { fulfillPlaylist } from "../../../utils/fulfillInfo";
 
 export class GetPlaylistsFromUserService {
     constructor(
@@ -13,14 +14,11 @@ export class GetPlaylistsFromUserService {
         if(user_id !== requester_id) {
             return this.publicPlaylists(playlists).map(playlist => ({
                 ...playlist,
-                songs_url: `${process.env.API_URL}/user/playlist/${playlist.id}`
+                songs_url: `${process.env.API_URL}/user/playlist/${playlist.idPlaylist}`
             }));
         }
 
-        const playlistsAndSongs = playlists.map(playlist => ({
-            ...playlist,
-            songs_url: `${process.env.API_URL}/user/playlist/${playlist.id}`
-        }));
+        const playlistsAndSongs = playlists.map(fulfillPlaylist);
         
         return playlistsAndSongs;
     }
