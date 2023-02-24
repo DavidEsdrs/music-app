2ac59ef1d04026cb40e8f9e7ebb355268d19d8c9
DavidEsdrs <carpina1983@gmail.com>
2023-02-24 01:32:46 -0300

    Use query schema and its middlewares

--- src/services/playlists/playlists.router.ts
+++ src/services/playlists/playlists.router.ts
@@ -1,18 +1,20 @@
-import { Router } from "express";
+import { query, Router } from "express";
 import { ensureAuthUser } from "../../middlewares/ensureAuthUser";
 import { ensureAuthUserSoft } from "../../middlewares/ensureAuthUserSoft";
+import { validateQuery } from "../../utils/validations";
 import { buildAddPlaylistToUser } from "./AddPlaylistToUser/buildAddPlaylistToUser";
 import { buildCreatePlaylist } from "./CreatePlaylist/buildCreatePlaylist";
 import { validateCreatePlaylist } from "./CreatePlaylist/CreatePlaylist.middleware";
 import { buildDeletePlaylist } from "./DeletePlaylist/buildDeletePlaylist";
 import { buildGetPlaylist } from "./GetPlaylist/buildGetPlaylist";
+import { getPlaylistSchema } from "./GetPlaylist/getPlaylist.middleware";
 import { buildGetPlaylistsFromUser } from "./GetPlaylistsFromUser/buildGetPlaylistsFromUser";
 
 const router = Router();
 
-router.get("/playlist/:id", ensureAuthUserSoft, (req, res) => buildGetPlaylist().handle(req, res));
+router.get("/playlist/:id", ensureAuthUserSoft, query({ plainObjects: true }), validateQuery(getPlaylistSchema), (req, res) => buildGetPlaylist().handle(req, res));
 
-router.get("/user/:user_id/playlist", ensureAuthUserSoft, (req, res) => buildGetPlaylistsFromUser().handle(req, res));
+router.get("/user/:user_id/playlist", ensureAuthUserSoft, query({ plainObjects: true }), validateQuery(getPlaylistSchema), (req, res) => buildGetPlaylistsFromUser().handle(req, res));
 
 router.post("/playlist", ensureAuthUser, validateCreatePlaylist, (req, res) => buildCreatePlaylist().handle(req, res));
 
