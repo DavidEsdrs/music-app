cd4c0e48f9ec78c46175888d8698b16570eb34fe
DavidEsdrs <carpina1983@gmail.com>
2023-03-02 15:08:48 -0300

    Add user creation body validation middleware

---
+++ src/services/users/CreateUser/createUserSchema.middleware.ts
@@ -0,0 +1,17 @@
+import Joi from "joi";
+
+export const createUserSchema = Joi.object({
+    username: Joi.
+        string().
+        min(2).
+        max(20),
+    email: Joi.
+        string().
+        email().
+        required(),
+
+    password: Joi.
+        string().
+        min(5).
+        required()
+});
--- src/services/users/users.router.ts
+++ src/services/users/users.router.ts
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import { ensureAuthUser } from "../../middlewares/ensureAuthUser";
+import { validateBody } from "../../utils/validations";
 import { buildCreateUser } from "./CreateUser/buildCreateUser";
+import { createUserSchema } from "./CreateUser/createUserSchema.middleware";
 import { buildLoginService } from "./LoginUser/buildLoginService";
 import { validateLoginUser } from "./LoginUser/LoginUser.middleware";
 
 const router = Router();
 
-router.post("/users", (req, res) => buildCreateUser().handle(req, res));
+router.post("/users", validateBody(createUserSchema), (req, res) => buildCreateUser().handle(req, res));
 router.post("/login", validateLoginUser, (req, res) => buildLoginService().handle(req, res));
 
 export { router as usersRouter };
