51572ca1c2d317ead4cc4e7e5b4a04931b6ba4ee
DavidEsdrs <carpina1983@gmail.com>
2023-03-02 15:10:18 -0300

    Add get user endpoint

--- src/services/users/users.router.ts
+++ src/services/users/users.router.ts
@@ -3,6 +3,7 @@ import { ensureAuthUser } from "../../middlewares/ensureAuthUser";
 import { validateBody } from "../../utils/validations";
 import { buildCreateUser } from "./CreateUser/buildCreateUser";
 import { createUserSchema } from "./CreateUser/createUserSchema.middleware";
+import { buildGetUser } from "./GetUser/buildGetUser";
 import { buildLoginService } from "./LoginUser/buildLoginService";
 import { validateLoginUser } from "./LoginUser/LoginUser.middleware";
 
@@ -10,5 +11,6 @@ const router = Router();
 
 router.post("/users", validateBody(createUserSchema), (req, res) => buildCreateUser().handle(req, res));
 router.post("/login", validateLoginUser, (req, res) => buildLoginService().handle(req, res));
+router.get("/get", (req, res) => buildGetUser().handle(req, res));
 
 export { router as usersRouter };
