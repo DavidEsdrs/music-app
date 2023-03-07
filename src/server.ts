0420155a04a78eaaf29fe12ad1e36d7e60974367
DavidEsdrs <carpina1983@gmail.com>
2023-03-07 01:26:55 -0300

    Add cors && '/api' before all endpoints

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,17 @@ import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
 import router from "./router";
+import cors from "cors";
 import { errorHandling } from "./middlewares/ErrorHandling";
 
 const server = express();
 
+server.use(cors({
+    origin: process.env.FRONT_END_URL
+}));
+
 server.use(express.json());
-server.use(router);
+server.use("/api", router);
 server.use(errorHandling);
 
 const SERVER_PORT = process.env.SERVER_PORT || 4343;
