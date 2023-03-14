034558b61f569853ab5e58c12d902f051deb9cb5
DavidEsdrs <carpina1983@gmail.com>
2023-03-14 00:54:01 -0300

    Config Cors && Cookie parser && HTTPS

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,39 @@
 import "reflect-metadata";
 import "express-async-errors";
 import express from "express";
+import fs from "fs";
 import router from "./router";
 import cors from "cors";
+import https from "https";
 import { errorHandling } from "./middlewares/ErrorHandling";
+import cookieParser from "cookie-parser";
 
 const server = express();
 
+server.use((req, res, next) => {
+    const origin = req.headers.origin;
+    if(origin === process.env.FRONT_END_URL) {
+        res.header("Access-Control-Allow-Credentials", "true");
+    }
+    next();
+});
+
 server.use(cors({
-    origin: process.env.FRONT_END_URL
+    origin: process.env.FRONT_END_URL,
+    credentials: true
 }));
 
 server.use(express.json());
+server.use(cookieParser());
+
 server.use("/api", router);
 server.use(errorHandling);
 
 const SERVER_PORT = process.env.SERVER_PORT || 4343;
 
-server.listen(SERVER_PORT, () => console.log(`running at ${SERVER_PORT}`));
+https.
+    createServer({
+        key: fs.readFileSync("localhost-key.pem"),
+        cert: fs.readFileSync("localhost.pem")
+    }, server).
+    listen(SERVER_PORT, () => console.log(`running at ${SERVER_PORT}`));
