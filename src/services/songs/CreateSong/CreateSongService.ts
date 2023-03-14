661d5b788e79487e73f9637b33e0b72064a20b1f
DavidEsdrs <carpina1983@gmail.com>
2023-03-14 01:02:30 -0300

    Returns fullfilled song object

--- src/services/songs/CreateSong/CreateSongService.ts
+++ src/services/songs/CreateSong/CreateSongService.ts
@@ -1,20 +1,21 @@
+import { Song } from "../../../entities/Song";
 import { IPlaylistsRepository } from "../../../repositories/PlaylistsRepository";
 import { ISongsRepository } from "../../../repositories/SongsRepository";
+import { FileHandling } from "../../../utils/fileHandling/FileHandling";
+import { fulfillSong } from "../../../utils/fulfillInfo";
 import { IAddSongToPlaylistService } from "../../playlists/AddSongToPlaylist/AddSongToPlaylistController";
 import { ICreateSongDTO } from "./CreateSongDTO";
 
 export class CreateSongService {
     constructor(
-        private songsRepository: ISongsRepository
+        private songsRepository: ISongsRepository,
+        private fileHandling: FileHandling
     ) {}
 
     async execute(args: ICreateSongDTO) {
         const song = this.songsRepository.create(args.song);
-        const songSaved = await this.songsRepository.saveSong(song);
-        const songWithDownload = {
-            ...songSaved,
-            download_link: `${process.env.API_URL}/song/${songSaved.idSong}/download`
-        };
-        return songWithDownload;
+        const songId = await this.songsRepository.saveSong(song);
+        const songInDb = await this.songsRepository.findById(songId);
+        return await fulfillSong(songInDb, this.fileHandling) as Song;
     }
 }
--- src/services/songs/CreateSong/buildCreateSong.ts
+++ src/services/songs/CreateSong/buildCreateSong.ts
@@ -1,11 +1,13 @@
 import { TypeormPlaylistsRepository as playlistsRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistsRepository";
 import { songsRepository } from "../../../repositories/typeorm/mysql/TypeormSongsRepository";
+import { FFmpegFileHandling } from "../../../utils/fileHandling/FFmpegFileHandling";
 import { AddSongToPlaylistService } from "../../playlists/AddSongToPlaylist/AddSongToPlaylistService";
 import { CreateSongController } from "./CreateSongController";
 import { CreateSongService } from "./CreateSongService"
 
 export const buildCreateSong = () => {
-    const service = new CreateSongService(songsRepository);
+    const fileHandling = new FFmpegFileHandling();
+    const service = new CreateSongService(songsRepository, fileHandling);
     const controller = new CreateSongController(service);
     return controller;
 }
