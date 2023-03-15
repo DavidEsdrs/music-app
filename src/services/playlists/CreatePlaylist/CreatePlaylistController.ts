cdee7b504668dd5c7c290b01e808d294c5a982e0
DavidEsdrs <carpina1983@gmail.com>
2023-03-15 18:05:48 -0300

    Default value to 'released_on' field

--- src/services/playlists/CreatePlaylist/CreatePlaylistController.ts
+++ src/services/playlists/CreatePlaylist/CreatePlaylistController.ts
@@ -14,9 +14,12 @@ export class CreatePlaylistController {
     async handle(req: Request, res: Response) {
         const body = req.body;
         const { user_id } = req;
+        const path_featured_picture = req.file_props.file_name;
         const playlist = await this.service.execute({
             ...body,
-            creator_fk: user_id
+            released_on: req.body.released_on ?? new Date().getUTCFullYear(),
+            creator_fk: user_id,
+            path_featured_picture
         });
         return res.json(playlist);
     }
