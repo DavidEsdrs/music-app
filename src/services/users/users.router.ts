7e91750b95358b011fa3238c49a51d17beea1019
DavidEsdrs <carpina1983@gmail.com>
2023-03-15 23:08:44 -0300

    Pass users repository as parameter to the refresh endpoint

--- src/services/users/users.router.ts
+++ src/services/users/users.router.ts
@@ -2,6 +2,7 @@ import cookieParser from "cookie-parser";
 import { Router } from "express";
 import { ensureAuthUser } from "../../middlewares/ensureAuthUser";
 import { TypeormRefreshTokenRepository } from "../../repositories/typeorm/mysql/MySqlRefreshTokenRepository";
+import { TypeormUsersRepository } from "../../repositories/typeorm/mysql/TypeormUsersRepository";
 import { validateBody } from "../../utils/validations";
 import { buildCreateUser } from "./CreateUser/buildCreateUser";
 import { createUserSchema } from "./CreateUser/createUserSchema.middleware";
@@ -16,7 +17,7 @@ const router = Router();
 router.post("/users", validateBody(createUserSchema), (req, res) => buildCreateUser().handle(req, res));
 router.post("/login", validateBody(loginSchema), (req, res) => buildLoginService().handle(req, res));
 router.get("/get", (req, res) => buildGetUser().handle(req, res));
-router.get("/refresh", (req, res) => new RefreshTokenController(TypeormRefreshTokenRepository).handle(req, res));
+router.get("/refresh", (req, res) => new RefreshTokenController(TypeormRefreshTokenRepository, TypeormUsersRepository).handle(req, res));
 router.get("/logout", (req, res) => new LogoutUserController(TypeormRefreshTokenRepository).handle(req, res));
 
 export { router as usersRouter };
