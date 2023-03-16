0d545a192b7c8d317d1a9c024d01944251965e1b
DavidEsdrs <carpina1983@gmail.com>
2023-03-15 23:08:08 -0300

    Uses validateBody function as validation middleware in login endpoint'

--- src/services/users/users.router.ts
+++ src/services/users/users.router.ts
@@ -7,14 +7,14 @@ import { buildCreateUser } from "./CreateUser/buildCreateUser";
 import { createUserSchema } from "./CreateUser/createUserSchema.middleware";
 import { buildGetUser } from "./GetUser/buildGetUser";
 import { buildLoginService } from "./LoginUser/buildLoginService";
-import { validateLoginUser } from "./LoginUser/LoginUser.middleware";
+import { loginSchema } from "./LoginUser/LoginUser.middleware";
 import { LogoutUserController } from "./LogoutUser/LogoutUserController";
 import { RefreshTokenController } from "./RefreshToken/RefreshTokenController";
 
 const router = Router();
 
 router.post("/users", validateBody(createUserSchema), (req, res) => buildCreateUser().handle(req, res));
-router.post("/login", validateLoginUser, (req, res) => buildLoginService().handle(req, res));
+router.post("/login", validateBody(loginSchema), (req, res) => buildLoginService().handle(req, res));
 router.get("/get", (req, res) => buildGetUser().handle(req, res));
 router.get("/refresh", (req, res) => new RefreshTokenController(TypeormRefreshTokenRepository).handle(req, res));
 router.get("/logout", (req, res) => new LogoutUserController(TypeormRefreshTokenRepository).handle(req, res));
