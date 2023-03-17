67cfd07ab1a389bd96ba1e6da835fb8bffef8a84
DavidEsdrs <carpina1983@gmail.com>
2023-03-17 06:25:39 -0300

    Parse tags repository to service instantiation

--- src/services/songs/CreateSong/buildCreateSong.ts
+++ src/services/songs/CreateSong/buildCreateSong.ts
@@ -1,5 +1,6 @@
 import { TypeormPlaylistsRepository as playlistsRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistsRepository";
 import { songsRepository } from "../../../repositories/typeorm/mysql/TypeormSongsRepository";
+import { TypeormTagRepository } from "../../../repositories/typeorm/mysql/TypeormTagRepository";
 import { FFmpegFileHandling } from "../../../utils/fileHandling/FFmpegFileHandling";
 import { AddSongToPlaylistService } from "../../playlists/AddSongToPlaylist/AddSongToPlaylistService";
 import { CreateSongController } from "./CreateSongController";
@@ -7,7 +8,7 @@ import { CreateSongService } from "./CreateSongService"
 
 export const buildCreateSong = () => {
     const fileHandling = new FFmpegFileHandling();
-    const service = new CreateSongService(songsRepository, fileHandling);
+    const service = new CreateSongService(songsRepository, TypeormTagRepository, fileHandling);
     const controller = new CreateSongController(service);
     return controller;
 }
