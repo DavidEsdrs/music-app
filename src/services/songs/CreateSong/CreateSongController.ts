c39a6a294277009c3d2102135b5773decdd01e5a
DavidEsdrs <carpina1983@gmail.com>
2023-03-17 06:38:32 -0300

    Check if file_name is null or undefined since send a file is required to create a son

--- src/services/songs/CreateSong/CreateSongController.ts
+++ src/services/songs/CreateSong/CreateSongController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { UnprocessableEntityError } from "../../../api/APIErrors";
 import { Song } from "../../../entities/Song";
 import { ICreateSongDTO } from "./CreateSongDTO";
 
@@ -13,7 +14,8 @@ export class CreateSongController {
 
     async handle(req: Request, res: Response) {
         const { title, tags } = req.body;
-        const file_path = req.file_props.file_name;
+        const file_path = req.file_props?.file_name;
+        if(!file_path) throw new UnprocessableEntityError("You must to send a file!");
         const { user_id: creator_fk } = req;
         const song = await this.service.execute({ creator_fk, file_path, title, tags });
         return res.json(song);
