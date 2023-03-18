7996614001a1c03b5230be908ce07dd09c493097
DavidEsdrs <carpina1983@gmail.com>
2023-03-18 11:33:17 -0300

    Add findSongAndPlaylistByTag function

--- src/repositories/TagRepository.ts
+++ src/repositories/TagRepository.ts
@@ -1,6 +1,14 @@
+import { Playlist } from "../entities/Playlist";
+import { Song } from "../entities/Song";
 import { Tag } from "../entities/Tag";
 import { GenericRepository } from "./GenericRepository";
 
+export type SongAndPlaylist = {
+    songs: Song[];
+    playlists: Playlist[];
+};
+
 export interface ITagRepository extends GenericRepository<Tag> {
     findByName(name: string): Promise<Tag>;
+    findSongAndPlaylistByTag(tag: string, limit: number): Promise<SongAndPlaylist[]>;
 }
