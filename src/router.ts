80f6c7b79e5dc1a7f2651b3b07fb771a9131ff74
DavidEsdrs <carpina1983@gmail.com>
2023-03-27 16:24:23 -0300

    Add and serve partial API documentation

--- src/router.ts
+++ src/router.ts
@@ -3,10 +3,15 @@ import { playlistsRouter } from "./services/playlists/playlists.router";
 import { songsRouter } from "./services/songs/songs.router";
 import { tagsRouter } from "./services/tags/tags.router";
 import { usersRouter } from "./services/users/users.router";
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "./swagger.json";
 
 const router = Router();
 
 router.get("/", (req, res) => res.json("running"));
+
+router.use("/docs", swaggerUi.serve);
+router.get("/docs", swaggerUi.setup(swaggerDocs));
 
 router.use(playlistsRouter);
 router.use(usersRouter);
