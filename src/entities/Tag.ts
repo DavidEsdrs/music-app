bfa7e2547fa96849c67972fbc7ffbb91038c7398
DavidEsdrs <carpina1983@gmail.com>
2023-03-28 23:52:48 -0300

    Split Tag entity into two entities to allow simpler queries

--- src/entities/Tag.ts
+++ src/entities/Tag.ts
@@ -1,18 +1,36 @@
 import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
 
-@Entity("tags")
-export class Tag {
+@Entity("song_tags")
+export class TagSong {
     @PrimaryGeneratedColumn()
-    idTag: number;
+    idSongTag: number;
+
+    @Column({ name: "name", type: "varchar", nullable: false })
+    name: string;
+
+    @Column({ name: "song_id", type: "integer" })
+    song_id: number;
+
+    @Column({ name: "type", type: "enum", enum: ["feature", "genre", "artist"] })
+    type: "feature" | "genre" | "artist";
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
+}
+
+@Entity("playlist_tags")
+export class TagPlaylist {
+    @PrimaryGeneratedColumn()
+    idPlaylistTag: number;
 
     @Column({ name: "name", type: "varchar", nullable: false })
     name: string;
 
     @Column({ name: "playlist_id", type: "integer" })
     playlist_id: number;
-
-    @Column({ name: "song_id", type: "integer" })
-    song_id: number;
 
     @Column({ name: "type", type: "enum", enum: ["feature", "genre", "artist"] })
     type: "feature" | "genre" | "artist";
