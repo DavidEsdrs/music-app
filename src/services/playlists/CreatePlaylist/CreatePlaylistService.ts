7057a5823764129ee615a6a8698cdf31232563a9
DavidEsdrs <carpina1983@gmail.com>
2023-03-30 01:41:25 -0300

    Refactor: Change variable names to improve readbility

--- src/services/playlists/CreatePlaylist/CreatePlaylistService.ts
+++ src/services/playlists/CreatePlaylist/CreatePlaylistService.ts
@@ -1,20 +1,20 @@
 import { Playlist } from "../../../entities/Playlist";
 import { TagPlaylist } from "../../../entities/Tag";
 import { IPlaylistsRepository } from "../../../repositories/PlaylistsRepository";
-import { ITagRepository } from "../../../repositories/TagRepository";
+import { ITagPlaylistRepository } from "../../../repositories/TagRepository";
 import { ICreatePlaylistDTO } from "./CreatePlaylistDTO";
 
 export class CreatePlaylistService {
     constructor(
         private playlistsRepository: IPlaylistsRepository,
-        private tagsRepository: ITagRepository
+        private tagsRepository: ITagPlaylistRepository
     ) {}
 
     async execute({ title, description, creator_fk, path_featured_picture, released_on, tags, visibility }: ICreatePlaylistDTO) {
         const playlist = this.playlistsRepository.create({ title, description, creator_fk, path_featured_picture, released_on, visibility });
         const createdPlaylistId = await this.playlistsRepository.savePlaylist(playlist);
-        const promises = tags.map(tag => this.saveTagPromise(tag, createdPlaylistId));
-        const tagsInDb = await Promise.all([...promises]);
+        const saveTagPromises = tags.map(tag => this.saveTagPromise(tag, createdPlaylistId));
+        const tagsInDb = await Promise.all([...saveTagPromises]);
         return {
             idPlaylist: createdPlaylistId,
             ...playlist,
@@ -23,7 +23,7 @@ export class CreatePlaylistService {
     }
 
     async saveTagPromise(tag: TagPlaylist, playlistId: number) {
-        const tagObj = this.tagsRepository.create({ name: tag.name, type: tag.type, playlist_id: playlistId });
+        const tagObj = this.tagsRepository.create({ name: tag.name, tag_type: tag.tag_type, playlist_id: playlistId });
         const tagInDb = await this.tagsRepository.save(tagObj);
         return tagInDb;
     }
