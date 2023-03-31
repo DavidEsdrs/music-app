c63b35cb1ce17b28734ec2ebc8c7b07bbc3d0e04
DavidEsdrs <carpina1983@gmail.com>
2023-03-31 19:56:17 -0300

    Change status code to 409 in DefaultPlaylistDeleteError to indicate that the user cannot delete their own default playlist since it's a data constraint violation

--- src/api/APIErrors.ts
+++ src/api/APIErrors.ts
@@ -64,7 +64,7 @@ export class DuplicateSongEntryError extends APIErrors {
 
 export class DefaultPlaylistDeleteError extends APIErrors {
     constructor() {
-        super(422, "Can't delete default playlist!");
+        super(409, "Can't delete default playlist!");
     }
 }
 
