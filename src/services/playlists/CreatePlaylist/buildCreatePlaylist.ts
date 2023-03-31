96e3c98d4606323cfeed30cd05b8b404c035e1e4
DavidEsdrs <carpina1983@gmail.com>
2023-03-31 19:38:37 -0300

    Update import from TagRepository to TagPlaylistRepository since the original tags table was splitted into two

--- src/services/playlists/CreatePlaylist/buildCreatePlaylist.ts
+++ src/services/playlists/CreatePlaylist/buildCreatePlaylist.ts
@@ -1,10 +1,10 @@
 import { TypeormPlaylistsRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistsRepository"
-import { TypeormTagRepository } from "../../../repositories/typeorm/mysql/TypeormTagRepository";
+import { TypeormTagPlaylistRepository } from "../../../repositories/typeorm/mysql/TypeormTagRepository";
 import { CreatePlaylistController } from "./CreatePlaylistController";
 import { CreatePlaylistService } from "./CreatePlaylistService"
 
 export const buildCreatePlaylist = () => {
-    const service = new CreatePlaylistService(TypeormPlaylistsRepository, TypeormTagRepository);
+    const service = new CreatePlaylistService(TypeormPlaylistsRepository, TypeormTagPlaylistRepository);
     const controller = new CreatePlaylistController(service);
     return controller;
 }
