84667ee3a8020808f22a998b5bd382e628ee335a
DavidEsdrs <carpina1983@gmail.com>
2023-03-31 23:25:45 -0300

    Split TypeormTagRepository into TypeormTagPlaylistRepository and TypeormTagSongRepository to separate functionality for playlist and song tags and match the new database schema

--- src/repositories/typeorm/mysql/TypeormTagRepository.ts
+++ src/repositories/typeorm/mysql/TypeormTagRepository.ts
@@ -1,14 +1,12 @@
 import { Playlist } from "../../../entities/Playlist";
 import { Song } from "../../../entities/Song";
-import { Tag } from "../../../entities/Tag";
+import { TagPlaylist, TagSong } from "../../../entities/Tag";
 import AppDataSource from "../../../ormconfig";
 import { SongAndPlaylist, TagSongOrPlaylist } from "../../TagRepository";
 
-
-
-export const TypeormTagRepository = AppDataSource.getRepository(Tag).extend({
-    async findById(idTag: number) {
-        const tag = await this.findOneBy({ idTag });
+export const TypeormTagPlaylistRepository = AppDataSource.getRepository(TagPlaylist).extend({
+    async findById(idPlaylistTag: number) {
+        const tag = await this.findOneBy({ idPlaylistTag });
         return tag;
     },
 
@@ -17,8 +15,8 @@ export const TypeormTagRepository = AppDataSource.getRepository(Tag).extend({
         return tag;
     },
 
-    async findSongAndPlaylistByTag(tag: string, limit: number = 20) {
-        const songsAndPlaylists = await this.query(`
+    async findPlaylistsByTag(tag: string, limit: number = 20) {
+        const playlists = await this.query(`
             SELECT 
                 t.idTag, 
                 t.name, 
@@ -33,24 +31,12 @@ export const TypeormTagRepository = AppDataSource.getRepository(Tag).extend({
                 p.created_at playlist_created_on, 
                 p.updated_at playlist_updated_at, 
                 p.creator_fk playlist_creator_fk, 
-                s.idSong, 
-                s.title song_title, 
-                s.file_path,
-                s.created_at song_created_at, 
-                s.updated_at song_updated_at, 
-                s.creator_fk song_creator_fk
-            FROM tags t
+            FROM playlist_tags t
             LEFT JOIN playlists p ON t.playlist_id=p.idPlaylist
-            LEFT JOIN songs s ON t.song_id=s.idSong
             WHERE t.name=?
             LIMIT ?
         `, [tag, limit]);
-        const sps: SongAndPlaylist = {
-            songs: [],
-            playlists: []
-        };
-        songsAndPlaylists.forEach((sp: any) => sp.idSong ? sps.songs.push(sp) : sps.playlists.push(sp));
-        return sps;
+        return playlists;
     },
     async findSongAndPlaylistByTags(tags: string[], limit: number = 10) {
         const songsAndPlaylists = await this.
@@ -73,13 +59,13 @@ export const TypeormTagRepository = AppDataSource.getRepository(Tag).extend({
     async getTagsFromSong(song_id: number) {
         const tagsFromSong = await this.query(`
             SELECT *
-            FROM tags t
-            WHERE t.song_id=?
+            FROM playlist_tags pt
+            WHERE pt.song_id=?
         `, [song_id]);
         return tagsFromSong;
     },
 
-    async countTagByUsage(tags: Tag[]) {
+    async countTagByUsage(tags: TagPlaylist[]) {
         const tagsCount = await this.
             createQueryBuilder("t").
             select("t.name", "name").
@@ -88,6 +74,81 @@ export const TypeormTagRepository = AppDataSource.getRepository(Tag).extend({
             where("t.name IN (:...tags)", { tags: tags.map(t => t.name) }).
             groupBy("t.name").
             addGroupBy("t.type").
+            orderBy("quantity", "DESC").
+            getRawMany();
+        return tagsCount.map(t => ({ ...t, quantity: Number(t.quantity) }));
+    }
+});
+
+export const TypeormTagSongRepository = AppDataSource.getRepository(TagSong).extend({
+    async findById(idSongTag: number) {
+        const tag = await this.findOneBy({ idSongTag });
+        return tag;
+    },
+
+    async findByName(name: string) {
+        const tag = await this.findOneBy({ name });
+        return tag;
+    },
+
+    async findPlaylistsByTag(tag: string, limit: number = 20) {
+        const playlists = await this.query(`
+            SELECT 
+                t.idTag, 
+                t.name, 
+                t.created_at, 
+                t.updated_at, 
+                p.idPlaylist, 
+                p.title playlist_title, 
+                p.description playlist_description, 
+                p.visibility playlist_visibility, 
+                p.path_featured_picture, 
+                p.released_on, 
+                p.created_at playlist_created_on, 
+                p.updated_at playlist_updated_at, 
+                p.creator_fk playlist_creator_fk, 
+            FROM song_tags t
+            LEFT JOIN playlists p ON t.playlist_id=p.idPlaylist
+            WHERE t.name=?
+            LIMIT ?
+        `, [tag, limit]);
+        return playlists;
+    },
+    async findSongAndPlaylistByTags(tags: string[], limit: number = 10) {
+        const songsAndPlaylists = await this.
+            createQueryBuilder("tag").
+            leftJoinAndSelect("songs", "song", "tag.song_id=song.idSong").
+            where("tag.name IN (:...tags)", { tags }).
+            limit(limit).
+            getRawMany();
+
+        const sps: TagSongOrPlaylist = {
+            songs: [],
+            playlists: []
+        };
+        
+        songsAndPlaylists.forEach((sp: any) => sp.song_idSong ? sps.songs.push(sp) : sps.playlists.push(sp));
+        return sps;
+    },
+
+    async getTagsFromSong(song_id: number) {
+        const tagsFromSong = await this.query(`
+            SELECT *
+            FROM song_tags t
+            WHERE t.song_id=?
+        `, [song_id]);
+        return tagsFromSong;
+    },
+
+    async countTagByUsage(tags: TagSong[]) {
+        const tagsCount = await this.
+            createQueryBuilder("t").
+            select("t.name", "name").
+            addSelect("t.tag_type", "type").
+            addSelect("COUNT(*)", "quantity").
+            where("t.name IN (:...tags)", { tags: tags.map(t => t.name) }).
+            groupBy("t.name").
+            addGroupBy("t.tag_type").
             orderBy("quantity", "DESC").
             getRawMany();
         return tagsCount.map(t => ({ ...t, quantity: Number(t.quantity) }));
