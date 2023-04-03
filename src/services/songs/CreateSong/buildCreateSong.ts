7d627f75f3e8dd1323ebbe63f9ae592cb012fd87
DavidEsdrs <carpina1983@gmail.com>
2023-04-03 12:57:53 -0300

    Refactor: Update tags repository import && Remove unnecessary imports

--- src/services/songs/CreateSong/buildCreateSong.ts
+++ src/services/songs/CreateSong/buildCreateSong.ts
@@ -1,14 +1,12 @@
-import { TypeormPlaylistsRepository as playlistsRepository } from "../../../repositories/typeorm/mysql/TypeormPlaylistsRepository";
 import { songsRepository } from "../../../repositories/typeorm/mysql/TypeormSongsRepository";
-import { TypeormTagRepository } from "../../../repositories/typeorm/mysql/TypeormTagRepository";
+import { TypeormTagSongRepository } from "../../../repositories/typeorm/mysql/TypeormTagRepository";
 import { FFmpegFileHandling } from "../../../utils/fileHandling/FFmpegFileHandling";
-import { AddSongToPlaylistService } from "../../playlists/AddSongToPlaylist/AddSongToPlaylistService";
 import { CreateSongController } from "./CreateSongController";
 import { CreateSongService } from "./CreateSongService"
 
 export const buildCreateSong = () => {
     const fileHandling = new FFmpegFileHandling();
-    const service = new CreateSongService(songsRepository, TypeormTagRepository, fileHandling);
+    const service = new CreateSongService(songsRepository, TypeormTagSongRepository, fileHandling);
     const controller = new CreateSongController(service);
     return controller;
 }
